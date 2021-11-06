a512bbfbd4dc79475a6a2ad5befcf87fab7aee08
Alexey Serous <Alexey.Serous@aalto.fi>
2021-11-06 22:10:10 +0200

    Added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Traffic Simulator Group 1
+ *
+ *        Version:  0.01
+ *        Created:  11/06/2021 08:47:25 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Authors:
+ *	   Member 1 Nikita Tarutin
+ *	   Member 2 Emma Rauhala
+ *	   Member 3 Nikolai Semin
+ *	   Member 4 Alexey Serous
+ *	   Member 5 Lauri Sivuoja
+ *
+ *   Organization:  Aalto
+ *
+ * =====================================================================================
+ */
+
+//standard library include
+#include <iostream>
+
+//local inlude
+#include "./include/CityObject.hpp"
+
+int main( int argc, char* argv[] )
+{
+	std::cout << "Hello, I am an empty main function for Traffic Simulator\n";
+
+	return 0;
+}
