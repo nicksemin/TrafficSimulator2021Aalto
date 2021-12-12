b53111a2912c1fe63f0f286678857fb668de3933
“”Nikolai <semin.nikolai@bk.ru>
2021-12-12 20:51:08 +0200

    Reverted the cityclass

--- src/include/cityclass.hpp
+++ src/include/cityclass.hpp
@@ -19,44 +19,31 @@
 #include "./include/Car.hpp"
 #include "./include/Navigator.hpp"
 #include "./include/Building.hpp"
-#include <QObject>
 
-class CityClass : public QObject
+class CityClass
 {
-    Q_OBJECT
-
-    public:
-        CityClass(std::string fileName);
-	~CityClass();
-        std::map<std::string, CrossroadClass*>& GetCrossroads();
-        std::vector<std::pair<std::string,RoadLineClass*>>& GetRoads();
-        std::vector<std::pair<std::string,Building*>>& GetRECBuildings();
-        std::vector<std::pair<std::string,Building*>>& GetRESBuildings();
-        std::vector<std::pair<std::string,Building*>>& GetINDBuildings();
-        std::vector<std::pair<std::string,Building*>>& GetCOMBuildings();
-        std::vector<std::pair<int, int>> crossroadsCoordinates_;
-        std::vector<std::pair<std::pair<int, int>,std::pair<int, int>>> roadsCoordinates_;
-        void sendCoords();
-
-signals:
-
-    void sendX(std::vector<std::pair<int, int>>);
-    void sendR( std::vector<std::pair<std::pair<int, int>,std::pair<int, int>>>);
-
-
+public:
+    CityClass(std::string fileName);
+    ~CityClass();
+    std::map<std::string, CrossroadClass*>& GetCrossroads();
+    std::vector<std::pair<std::string,RoadLineClass*>>& GetRoads();
+    std::vector<std::pair<std::string,Building*>>& GetRECBuildings();
+    std::vector<std::pair<std::string,Building*>>& GetRESBuildings();
+    std::vector<std::pair<std::string,Building*>>& GetINDBuildings();
+    std::vector<std::pair<std::string,Building*>>& GetCOMBuildings();
 
 
 protected:
 
-    private:
-	std::string m_fileName;
-	std::map<std::string, CrossroadClass*> m_crossroads;
-	std::vector<std::string> m_buildingExitCrossroadsNames;
-	std::vector<std::pair<std::string, RoadLineClass*>> m_roads;
-	std::vector<std::pair<std::string,Building*>> m_RECbuildings;
-	std::vector<std::pair<std::string,Building*>> m_RESbuildings;
-	std::vector<std::pair<std::string,Building*>> m_INDbuildings;
-	std::vector<std::pair<std::string,Building*>> m_COMbuildings;
+private:
+    std::string m_fileName;
+    std::map<std::string, CrossroadClass*> m_crossroads;
+    std::vector<std::string> m_buildingExitCrossroadsNames;
+    std::vector<std::pair<std::string, RoadLineClass*>> m_roads;
+    std::vector<std::pair<std::string,Building*>> m_RECbuildings;
+    std::vector<std::pair<std::string,Building*>> m_RESbuildings;
+    std::vector<std::pair<std::string,Building*>> m_INDbuildings;
+    std::vector<std::pair<std::string,Building*>> m_COMbuildings;
 };
 
 #endif // CITYCLASS_
