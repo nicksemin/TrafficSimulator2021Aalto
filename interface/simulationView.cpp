36ac701f96e6e6a871bdda2ca5bff7dd1e23a245
“”Nikolai <semin.nikolai@bk.ru>
2021-12-12 23:13:19 +0200

    fixed again...

--- interface/simulationView.cpp
+++ interface/simulationView.cpp
@@ -5,7 +5,7 @@
 // You may need to build the project (run Qt uic code generator) to get "ui_SimulationView.h" resolved
 
 #include "simulationView.h"
-#include "ui_SimulationView.h"
+#include "ui_simulationView.h"
 #include "./ui_simulationView.h"
 #include <QPainter>
 #include <QGraphicsScene>
